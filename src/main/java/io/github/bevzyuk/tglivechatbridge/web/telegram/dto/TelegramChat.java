4abd9f401b68fb4281e916a33a95dbffd9707558
BevzyukIvan13 <vanozero05@gmail.com>
2026-03-05 11:34:47 +0200

    Deleted TelegramChat

--- src/main/java/io/github/bevzyuk/tglivechatbridge/web/telegram/dto/TelegramChat.java
+++
@@ -1,6 +0,0 @@
-package io.github.bevzyuk.tglivechatbridge.web.telegram.dto;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record TelegramChat(Long id) { }
